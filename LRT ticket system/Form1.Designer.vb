ebac56db80d96b0033f623512b11c0bef611a3a1
Meeko11 <162523217+Meeko11@users.noreply.github.com>
2024-09-26 17:09:43 +0800

    Add files via upload

---
+++ LRT ticket system/Form1.Designer.vb
@@ -0,0 +1,226 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DT = New System.Windows.Forms.Label()
+        Me.TM = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(31, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(115, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Starting Point:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(298, 39)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 18)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "End Point:"
+        '
+        'DT
+        '
+        Me.DT.AutoSize = True
+        Me.DT.Location = New System.Drawing.Point(19, 10)
+        Me.DT.Name = "DT"
+        Me.DT.Size = New System.Drawing.Size(39, 13)
+        Me.DT.TabIndex = 2
+        Me.DT.Text = "Label3"
+        '
+        'TM
+        '
+        Me.TM.AutoSize = True
+        Me.TM.Location = New System.Drawing.Point(19, 40)
+        Me.TM.Name = "TM"
+        Me.TM.Size = New System.Drawing.Size(39, 13)
+        Me.TM.TabIndex = 3
+        Me.TM.Text = "Label4"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.TM)
+        Me.Panel1.Controls.Add(Me.DT)
+        Me.Panel1.Location = New System.Drawing.Point(276, 292)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(154, 63)
+        Me.Panel1.TabIndex = 4
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NumericUpDown1.Location = New System.Drawing.Point(283, 109)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(74, 29)
+        Me.NumericUpDown1.TabIndex = 5
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"--Select Location--", "Dagupan City ", "Urdaneta City ", "Rosales", "Paniqui", "Moncada ", "Conception", "Capas", "Bamban", "Mabalacat City ", "Angles city ", ""})
+        Me.ComboBox1.Location = New System.Drawing.Point(34, 75)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 23)
+        Me.ComboBox1.TabIndex = 6
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Items.AddRange(New Object() {"Regular ", "Goverment Employee (10%)", "Student (15%)", "Senior Citezen (20%)", "Children(50%)"})
+        Me.ComboBox3.Location = New System.Drawing.Point(196, 159)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox3.TabIndex = 8
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(86, 220)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(130, 50)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Procede"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(252, 220)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(95, 50)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"--Select Location--", "Dagupan City ", "Urdaneta City ", "Rosales", "Paniqui", "Moncada ", "Conception", "Capas", "Bamban", "Mabalacat City ", "Angles city ", ""})
+        Me.ComboBox2.Location = New System.Drawing.Point(276, 75)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 23)
+        Me.ComboBox2.TabIndex = 11
+        '
+        'Timer1
+        '
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(95, 158)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 18)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Discount:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(95, 115)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(182, 18)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Number of Passenger :"
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.FromArgb(CType(CType(240, Byte), Integer), CType(CType(53, Byte), Integer), CType(CType(53, Byte), Integer))
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.White
+        Me.Button3.Location = New System.Drawing.Point(2, 319)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(54, 36)
+        Me.Button3.TabIndex = 14
+        Me.Button3.Text = "Exit "
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(429, 356)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents DT As Label
+    Friend WithEvents TM As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button3 As Button
+End Class
---
+++ LRT ticket system/Form1.vb
@@ -0,0 +1,139 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Form2.Label10.Text = Me.ComboBox1.SelectedItem
+        Form2.Label11.Text = Me.ComboBox2.SelectedItem
+        Form2.Label12.Text = Me.NumericUpDown1.Value
+        Form2.Label1.Text = Format(Date.Now, "MMM dd , yyyy")
+        Form2.Label2.Text = Format(Date.Now, "Long Time")
+        Form2.Label14.Text = Me.ComboBox3.SelectedItem
+        Form2.Label15.Text = ""
+
+
+        Me.Hide()
+        Form2.Show()
+
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+        Me.DT.Text = Format(Date.Now, "MMM dd , yyyy")
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Me.TM.Text = Format(Date.Now, "long time")
+
+
+
+
+    End Sub
+
+    Private Sub ComboBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox3.SelectedIndexChanged
+        Dim discount As Double
+
+
+        If ComboBox3.SelectedIndex = 0 Then
+
+        End If
+        'COV
+        If ComboBox3.SelectedIndex = 1 Then
+            discount = 0.1
+        End If
+        'Student 
+        If ComboBox3.SelectedIndex = 2 Then
+            discount = 0.15
+        End If
+
+        'Senior citizen
+        If ComboBox3.SelectedIndex = 3 Then
+
+            discount = 0.2
+        End If
+
+        'Children
+        If ComboBox3.SelectedIndex = 3 Then
+            discount = 0.5
+        End If
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Dim price As Double
+
+        If ComboBox1.SelectedIndex = 0 Then
+
+        End If
+
+        If ComboBox1.SelectedIndex = 1 Then
+            price = 20
+
+        End If
+
+        If ComboBox1.SelectedIndex = 2 Then
+            price = 20
+
+        End If
+
+
+        If ComboBox1.SelectedIndex = 3 Then
+            price = 20
+
+        End If
+
+        If ComboBox1.SelectedIndex = 4 Then
+            price = 20
+
+        End If
+
+        If ComboBox1.SelectedIndex = 5 Then
+            price = 20
+
+        End If
+
+        If ComboBox1.SelectedIndex = 6 Then
+            price = 20
+
+        End If
+
+
+        If ComboBox1.SelectedIndex = 7 Then
+            price = 20
+
+        End If
+
+        If ComboBox1.SelectedIndex = 8 Then
+            price = 20
+
+        End If
+
+        If ComboBox1.SelectedIndex = 9 Then
+            price = 20
+
+        End If
+
+        If ComboBox1.SelectedIndex = 10 Then
+            price = 20
+
+        End If
+
+        If ComboBox1.SelectedIndex = 11 Then
+            price = 20
+
+        End If
+
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+End Class
